e2d7a3f1e690e93ddbdcdb7869e85190b2597552
Murik1502 <nikita.murashka@gmail.com>
2024-04-24 18:20:58 +0300

    style

--- core/utils/statesform.py
+++ core/utils/statesform.py
@@ -1,4 +1,5 @@
 from aiogram.fsm.state import StatesGroup
 
+
 class StatesForm(StatesGroup):
     pass
--- main.py
+++ main.py
@@ -6,22 +6,26 @@ from core.utils.commands import set_commands
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from cache.apsched import periodic
 
+
 async def start_bot(bot: Bot):
     await set_commands(bot)
     await bot.send_message(settings.bots.admin_id, text="Start")
 
+
 async def stop_bot(bot: Bot):
     await bot.send_message(settings.bots.admin_id, text="Stop")
+
 
 def start_sched(bot: Bot):
     scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
     scheduler.add_job(periodic, trigger='interval', seconds=60, kwargs={'bot': bot})
     scheduler.start()
-async def start():
 
+
+async def start():
     logging.basic_colorized_config(level=logging.DEBUG,
                                    format="%(astime)s - [%(levelname)s] - %(name)s - "
-                                   "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s")
+                                          "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s")
 
     bot = Bot(token=settings.bots.bot_token)
     dp = Dispatcher()
@@ -35,6 +39,7 @@ async def start():
         logging.error(f"EXCEPTION: {ex}", exc_info=True)
     finally:
         await bot.session.close()
+
 
 if __name__ == "__main__":
     asyncio.run(start())
