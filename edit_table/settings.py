3e89ee83d32dfc83e440f1d1f57d6164bccae0be
r0mch1ck <roma.kovalev2005@mail.ru>
2024-04-25 00:30:11 +0300

    upd: getenv

--- edit_table/settings.py
+++ edit_table/settings.py
@@ -2,12 +2,24 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 from peewee import PostgresqlDatabase
+import os
 
 
 scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive"]
 
-pg_db = PostgresqlDatabase('fsgnevents', user='postgres', password='12345', host='45.12.75.8', port=5438)
+database_name = os.getenv('DATABASE_NAME', 'default_db_name')
+database_user = os.getenv('DATABASE_USER', 'default_user')
+database_password = os.getenv('DATABASE_PASSWORD', 'default_password')
+database_host = os.getenv('DATABASE_HOST', 'localhost')
+database_port = os.getenv('DATABASE_PORT', 5432)
 
+pg_db = PostgresqlDatabase(
+    database_name,
+    user=database_user,
+    password=database_password,
+    host=database_host,
+    port=database_port
+)
 pg_db.connect()
 
 query = 'SELECT * FROM googleapi'
