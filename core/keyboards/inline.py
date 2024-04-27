cd1a4248645a47071d442163c39b2fd24e49fa8f
Murik1502 <nikita.murashka@gmail.com>
2024-04-27 03:29:01 +0300

    feat: registration buttons

--- core/keyboards/inline.py
+++ core/keyboards/inline.py
@@ -1,7 +1,11 @@
 from aiogram.types import *
 
+join_event = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Вступить", callback_data="event_id"),
+     ]
+], )
 
-join_event= InlineKeyboardMarkup(inline_keyboard=[
-[InlineKeyboardButton(text = "Вступить", callback_data="event_id"),
-]
-],)
+reg_status = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Всё верно", callback_data="confirm"),
+     InlineKeyboardButton(text="Изменить", callback_data="change")]
+], )
