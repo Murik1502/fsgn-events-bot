694d5c876ce4316544a5e56ef63ebef20f7ee4e4
Your Name <you@example.com>
2024-04-27 18:08:09 +0300

    add inline for new_event handlers

--- core/keyboards/inline.py
+++ core/keyboards/inline.py
@@ -5,6 +5,16 @@ join_event = InlineKeyboardMarkup(inline_keyboard=[
      ]
 ], )
 
+event_status = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Сохранить", callback_data="add event"),
+     InlineKeyboardButton(text="Пересоздать", callback_data="create again")]
+], )
+
+event_type = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Командное", callback_data="team"),
+     InlineKeyboardButton(text="Одиночное", callback_data="individual")]
+], )
+
 reg_status = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Всё верно", callback_data="confirm"),
      InlineKeyboardButton(text="Изменить", callback_data="change")]
