8c4af271baf4ed0be6e87d0bc9ba51fbd122ddbc
Your Name <you@example.com>
2024-05-25 22:05:47 +0300

    add: to_excel button for admins

--- core/keyboards/reply.py
+++ core/keyboards/reply.py
@@ -1,5 +1,6 @@
 from aiogram.types import *
 
 admin_keyword = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="/new_event",),]
+    [KeyboardButton(text="/new_event",)],
+    [KeyboardButton(text="/to_excel",)]
 ], resize_keyboard=True)
